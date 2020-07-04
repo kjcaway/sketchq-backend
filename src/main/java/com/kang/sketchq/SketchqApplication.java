d5bf653f3152953d0f4a22e7844c059fa4a9f777
kjcaway <whdckstngh@naver.com>
2020-07-04 13:48:50 +0900

    first

---
+++ src/main/java/com/kang/sketchq/SketchqApplication.java
@@ -0,0 +1,13 @@
+package com.kang.sketchq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SketchqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SketchqApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kang/sketchq/SketchqApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kang.sketchq;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SketchqApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
