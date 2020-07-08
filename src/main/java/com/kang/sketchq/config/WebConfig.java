e96b41f2920808c3cc3340cc0c0f230f520db652
kjcaway <whdckstngh@naver.com>
2020-07-09 00:09:24 +0900

    cors

---
+++ src/main/java/com/kang/sketchq/config/WebConfig.java
@@ -0,0 +1,17 @@
+package com.kang.sketchq.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.config.CorsRegistry;
+import org.springframework.web.reactive.config.EnableWebFlux;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+
+@Configuration
+@EnableWebFlux
+public class WebConfig implements WebFluxConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*") // any host or put domain(s) here
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTION"); // put the http verbs you want allow
+    }
+}
