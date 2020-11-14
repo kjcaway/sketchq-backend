3d2dc0d245efaf7f3275c157270af83eb8c95286
kjcaway <whdckstngh@naver.com>
2020-11-14 12:52:09 +0900

    ** 테스트코드 주석처리

--- src/test/java/com/kang/sketchq/SketchqApplicationTests.java
+++ src/test/java/com/kang/sketchq/SketchqApplicationTests.java
@@ -1,16 +1,11 @@
 package com.kang.sketchq;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-
-@RunWith(SpringRunner.class)
-@SpringBootTest
-public class SketchqApplicationTests {
-
-	@Test
-	public void contextLoads() {
-	}
-
-}
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+//public class SketchqApplicationTests {
+//
+//	@Test
+//	public void contextLoads() {
+//	}
+//
+//}
